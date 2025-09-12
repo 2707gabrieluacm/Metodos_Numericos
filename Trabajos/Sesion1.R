e86b2c17c171d1a38c8f03e6884c3d755cdcc8af
2707gabrieluacm <gabriel.ortiz.cruz@estudiantxs.uacm.edu.mx>
2025-09-11 21:30:27 -0600

    Carpeta para almacenar los documentos de la clase

---
+++ Trabajos/Sesion1.R
@@ -0,0 +1,81 @@
+#---- SESION 1 ----
+##---- OPERADORES LOGICOS ----
+17<5
+17>5
+17<=5
+17>=5
+17!=5
+17==5
+##---- OPERADORES ARITMETICOS ----
+# SUMA, RESTA, MULTIPLICACION, DIVISION, POTENCIA, MODULO, DIVISION ENTERA
+17+5
+17*5
+17*5
+17^5
+17%/%5
+17%%5
+##---- LOGARITMOS Y EXPONENCIALES ----
+log(1)
+log(12)
+log(12,2)
+exp(12)
+exp(1)
+##---- FUNCIONES TRIGONOMETRICAS ----
+sin(45 * pi / 180) # Convertir grados a radianes
+cos(45 * pi / 180) # Convertir grados a radianes
+tan(45 * pi / 180) # Convertir grados a radianes
+asin(0.96)
+acos(0.97)
+atan(0.45)
+##---- FUNCIONES VARIAS ----
+abs(-34)
+sqrt(8)
+floor(1.56)
+ceiling(1.56)
+max(4,7,2,12)
+min(4,7,2,12)
+sign(-45)
+##---- EJERCICIOS DE PRACTICA ----
+# 1. calcular la expresion cos(pi/6+pi/2)+e^2
+cos(pi/6 + pi/2) + exp(1)^2
+
+# 2. calcular la expresion cos(pi/6+pi/2)+e^2*log(5)+arc cos(1/raiz(2))
+cos(pi/6 + pi/2) + exp(1)^2 * log(5) + acos(1/sqrt(2))
+
+# 3. introducir las siguientes expresiones:
+# a) 1/7
+1/7
+
+# b) options(digits=3); 1/7
+options(digits = 3); 1/7
+
+# c) options(digits=6); 1/7
+options(digits = 6); 1/7
+
+# d) round(67.45)
+round(67.45)
+
+# e) round(75.324568,2)
+round(75.324568, 2)
+
+# f) options(digits=7);
+options(digits = 7); # No es necesario poner 1, solo se cambia la opción
+
+# g) signif(56.345458234234,2)
+signif(56.345458234234, 2)
+
+# h) signif(56.345458234234)
+signif(56.345458234234)
+
+# i) exp(-30)
+exp(-30)
+
+# j) options(scipen= 999)
+options(scipen = 999)
+
+# k) exp(-30)
+exp(-30)
+
+# l) options(scipen=0)
+options(scipen = 0)
+
